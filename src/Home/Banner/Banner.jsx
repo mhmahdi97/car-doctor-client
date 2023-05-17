3fb3d59a686fa6f430876c3e1bb9a3ae3c8d4224
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-17 18:08:44 +0600

    partially finished slider design

---
+++ src/Home/Banner/Banner.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import img1 from '../../assets/images/banner/1.jpg';
+import img2 from '../../assets/images/banner/2.jpg';
+import img3 from '../../assets/images/banner/3.jpg';
+import img4 from '../../assets/images/banner/4.jpg';
+import img5 from '../../assets/images/banner/5.jpg';
+import img6 from '../../assets/images/banner/6.jpg';
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[600px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full rounded-xl" />
+                <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                    <div className='w-1/3 ml-24'>
+                        <h1 className='text-6xl text-white font-bold mb-7'>Affordable Price For Car Servicing</h1>
+                        <p className='mb-6 text-lg text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <button className='btn btn-primary mr-5'>Discover More</button>
+                            <button className='btn btn-outline btn-secondary'>Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img3} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div> 
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img4} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img5} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img6} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+                <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
